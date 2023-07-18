98b0194f2a1452aff7df2bd9026b30e711ca4141
Kim Kreiss <kimberlymkreiss@gmail.com>
2023-07-18 15:32:15 -0700

    file structure/changes & edits to down_load.sh to add GRACE mascons

--- scripts/data/download_data.sh
+++ scripts/data/download_data.sh
@@ -10,3 +10,5 @@ podaac-data-downloader -c TELLUS_GRFO_L3_GFZ_RL06.1_LND_v04 -d ~/remoteData/GRAC
 
 podaac-data-downloader -c TELLUS_GRAC_L3_JPL_RL06_LND_v04 -d ~/remoteData/GRACE_NEW2/TELLUS_GRAC_L3_JPL_RL06_LND_v04 --start-date 2002-04-05T00:00:00Z --end-date 2017-10-19T00:00:00Z
 podaac-data-downloader -c TELLUS_GRFO_L3_JPL_RL06.1_LND_v04 -d ~/remoteData/GRACE_NEW2/TELLUS_GRFO_L3_JPL_RL06.1_LND_v04 --start-date 2018-05-22T00:00:00Z --end-date 2023-06-23T00:00:00Z
+
+podaac-data-subscriber -c TELLUS_GRAC-GRFO_MASCON_CRI_GRID_RL06.1_V3 -d ~/remoteData/TELLUS_GRAC-GRFO_MASCON_CRI_GRID_RL06.1_V3 --start-date 2002-04-04T00:00:00Z --end-date 2023-09-01T00:00:00Z
